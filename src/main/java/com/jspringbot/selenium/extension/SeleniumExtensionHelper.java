8edf2a37ae24dc5d0f44d9b216ebc289fc9e0dc1
robertdeocampojr <robertdeocampojr@gmail.com>
2018-03-22 14:11:40 +0800

    support selenium 3.0

--- src/main/java/com/jspringbot/selenium/extension/SeleniumExtensionHelper.java
+++ src/main/java/com/jspringbot/selenium/extension/SeleniumExtensionHelper.java
@@ -6,11 +6,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -30,6 +27,10 @@ public class SeleniumExtensionHelper{
         this.driver = driver;
         this.executor = (JavascriptExecutor) driver;
         this.finder = new ElementFinder(driver);
+    }
+
+    public void closeSession(){
+        this.driver.quit();
     }
 
     public void highlightElement(String locator){
